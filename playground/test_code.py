8533fccd66b990b07b41002080a1ce4461c715f5
ChiragBellara <chiragbellara7@gmail.com>
2026-01-22 11:55:10 -0800

    Fixed Linter errors and added code to write the output to a text file.

--- playground/test_code.py
+++ playground/test_code.py
@@ -1,13 +1,49 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler
+from pathlib import Path
+from typing import cast
+from crawl4ai import (AsyncWebCrawler, CrawlerRunConfig,
+                      DefaultMarkdownGenerator, PruningContentFilter, CacheMode)
+from crawl4ai.models import CrawlResult
 
 
-async def run_crawler():
+class FileWriteError(RuntimeError):
+    pass
+
+
+async def main():
+    crawl_config = CrawlerRunConfig(
+        # cache_mode=CacheMode.ENABLED,
+        markdown_generator=DefaultMarkdownGenerator(
+            content_filter=PruningContentFilter(),
+            options={
+                "ignore_links": True,
+
+            }
+        ),
+    )
     async with AsyncWebCrawler() as web_crawler:
-        result = await web_crawler.arun(
+        result = cast(CrawlResult, await web_crawler.arun(
             url="https://www.anthropic.com",
-        )
-        print(result.markdown)
+            config=crawl_config
+        ))
+        if result.markdown:
+            print(len(result.markdown.raw_markdown))
+            print(len(result.markdown.fit_markdown))
+            if result.markdown.fit_markdown:
+                return result.markdown.fit_markdown
+        return None
+
+
+def write_to_file(content, file_to_write):
+    try:
+        path = Path(file_to_write)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_text(content, encoding="utf-8")
+    except Exception as ex:
+        raise FileWriteError(f"Failed to write to {path}")
+
 
 if __name__ == "__main__":
-    asyncio.run(run_crawler())
+    site_content = asyncio.run(main())
+    if site_content:
+        write_to_file(site_content, "../data/output.txt")
