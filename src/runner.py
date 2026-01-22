e0647e74353c3ab2922dc4a8a4e2849a15e0eef3
ChiragBellara <chiragbellara7@gmail.com>
2026-01-22 15:58:38 -0800

    Streamlined the extraction and filtering of markdown files.

--- src/runner.py
+++ src/runner.py
@@ -1,23 +1,14 @@
 import asyncio
-from scrapper import crawl_url, crawl_url_with_llm, write_to_file
-from processor import IdentityProcessor
+from scrapper import CrawlURLs
 from pathlib import Path
+import json
 
-FILE_PATH = Path("../data/output.txt")
-FILE_PATH_WITH_LLM_EXTRACTION = Path("../data/output_llm.txt")
-URL_TO_CRAWL = "https://www.anthropic.com/company"
+crawler = CrawlURLs()
+with open('./sources.json', 'r') as source_file:
+    sources = json.load(source_file)
 
-site_content = asyncio.run(crawl_url_with_llm(URL_TO_CRAWL))
-if site_content:
-    print(site_content)
-    write_to_file(site_content, FILE_PATH_WITH_LLM_EXTRACTION)
-
-if not FILE_PATH.is_file():
-    site_content = asyncio.run(crawl_url(URL_TO_CRAWL))
-    if site_content:
-        print("\nWriting content to file")
-        write_to_file(site_content, FILE_PATH)
-
-test = IdentityProcessor(provider="gpt-4o-mini", file_to_read=FILE_PATH)
-outcome = test._call_llm()
-print(outcome)
+    for src in sources:
+        print(f"Working on extracting data for {src["name"]}")
+        site_content = asyncio.run(crawler._crawl_url(src["url"]))
+        if site_content:
+            crawler.write_to_file(site_content, Path(src["storage_path"]))
--- src/scrapper.py
+++ src/scrapper.py
@@ -2,7 +2,7 @@ import os
 from typing import cast
 from company_identity import CompanyIdentity
 from crawl4ai import (AsyncWebCrawler, CrawlerRunConfig,
-                      DefaultMarkdownGenerator, LLMConfig, LLMExtractionStrategy, PruningContentFilter, CacheMode)
+                      DefaultMarkdownGenerator, LLMConfig, LLMExtractionStrategy, PruningContentFilter)
 from crawl4ai.models import CrawlResult
 
 
@@ -10,66 +10,50 @@ class FileWriteError(RuntimeError):
     pass
 
 
-async def crawl_url(URL_TO_CRAWL: str = ""):
-    crawl_config = CrawlerRunConfig(
-        # cache_mode=CacheMode.ENABLED,
-        markdown_generator=DefaultMarkdownGenerator(
+class CrawlURLs:
+    def __init__(self, model="openai"):
+        self.model = model
+
+    def _get_llm_config(self):
+        # Add functionality for the user to choose their model from a dropdown and update the LLM Config accordingly
+        return LLMConfig(
+            provider="openai/gpt-4o-mini", api_token=os.getenv("OPENAI_API_KEY")
+        )
+
+    def _get_mardown_generator(self):
+        return DefaultMarkdownGenerator(
             content_filter=PruningContentFilter(),
             options={
                 "ignore_links": True,
-
             }
-        ),
-    )
-    async with AsyncWebCrawler() as web_crawler:
-        result = cast(CrawlResult, await web_crawler.arun(
-            url=URL_TO_CRAWL,
-            config=crawl_config
-        ))
-        if result.markdown:
-            print(
-                f"Raw markdown content size: {len(result.markdown.raw_markdown)}")
-            print(
-                f"Content size after pruning: {len(result.markdown.fit_markdown)}")
-            if result.markdown.fit_markdown:
-                return result.markdown.fit_markdown
-        return None
+        )
 
-
-async def crawl_url_with_llm(URL_TO_CRAWL: str = ""):
-    crawl_config = CrawlerRunConfig(
-        # cache_mode=CacheMode.ENABLED,
-        word_count_threshold=1,
-        extraction_strategy=LLMExtractionStrategy(
-            llm_config=LLMConfig(
-                provider="openai/gpt-4o-mini", api_token=os.getenv("OPENAI_API_KEY")
-            ),
+    def _get_extraction_stratergy(self):
+        return LLMExtractionStrategy(
+            llm_config=self._get_llm_config(),
             schema=CompanyIdentity.model_json_schema(),
             extraction_type="schema",
             instruction="Analyze the markdown and extract the core business identity."
         ),
-        markdown_generator=DefaultMarkdownGenerator(
-            content_filter=PruningContentFilter(),
-            options={
-                "ignore_links": True,
 
-            }
+    async def _crawl_url(self, URL_TO_CRAWL: str = ""):
+        crawl_config = CrawlerRunConfig(
+            markdown_generator=self._get_mardown_generator(),
+            extraction_strategy=self._get_extraction_stratergy()[0]
         )
-    )
-    async with AsyncWebCrawler() as web_crawler:
-        result = cast(CrawlResult, await web_crawler.arun(
-            url=URL_TO_CRAWL,
-            config=crawl_config
-        ))
-        if result.extracted_content:
-            print(f"Content Size: {len(result.extracted_content)}")
-            return result.extracted_content
+        async with AsyncWebCrawler() as web_crawler:
+            result = cast(CrawlResult, await web_crawler.arun(
+                url=URL_TO_CRAWL,
+                config=crawl_config
+            ))
+            if result.extracted_content:
+                print(f"Content Size: {len(result.extracted_content)}")
+                return result.extracted_content
         return None
 
-
-def write_to_file(content, path):
-    try:
-        path.parent.mkdir(parents=True, exist_ok=True)
-        path.write_text(content, encoding="utf-8")
-    except Exception as ex:
-        raise FileWriteError(f"Failed to write to {path}")
+    def write_to_file(self, content, path):
+        try:
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.write_text(content, encoding="utf-8")
+        except Exception as ex:
+            raise FileWriteError(f"Failed to write to {path}")
